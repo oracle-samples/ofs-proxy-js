0750e955e84752c77828116d46d1a591b0d3a3a9
Borja Toron-Antons <borja.toron@oracle.com>
2023-07-18 13:20:29 -0400

    Adjust documentation

--- docs/sample/sample.js
+++ docs/sample/sample.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022, 2023, Oracle and/or its affiliates.
+ * Copyright © 2023, Oracle and/or its affiliates.
  * Licensed under the Universal Permissive License (UPL), Version 1.0  as shown at https://oss.oracle.com/licenses/upl/
  */
 
