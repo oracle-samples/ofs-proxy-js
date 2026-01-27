441070ea1f10fee6713da9170d430f6e3e12f1e9
Borja Toron <borja.toron@gmail.com>
2026-01-27 08:00:41 -0500

    Add CommonJS support and update package configuration

---
+++ rollup.config.cjs.mjs
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2022, 2023, Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License (UPL), Version 1.0  as shown at https://oss.oracle.com/licenses/upl/
+ */
+
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+
+export default {
+    input: "src/OFS.ts",
+    output: {
+        name: "OFS",
+        file: "dist/ofs.cjs",
+        format: "cjs",
+        exports: "named",
+    },
+    plugins: [
+        typescript(),
+        terser({
+            compress: {
+                unsafe: true,
+            },
+            mangle: true,
+            keep_fnames: true,
+            keep_classnames: true,
+        }),
+    ],
+};
